2a28aa92d404f9506db5ca386ab8056a2419f93d
Fox <kaitiefox@gmail.com>
2022-05-13 12:34:11 -0400

    optimistic updates

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -25,19 +25,50 @@ const addSuperHero = (hero) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    //steps to not making an additional network request once mutation succeeds:
-    // step 1: use param 'data' (which is the entire response for post request)
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries("super-heroes"); < this is a way to refetch data as soon as the mutation is successful
-      // however, this is an additional network request.
-      // to save a network request we have to do something else.
-      //step 2: update query cache using setQueryData. oldQueryData = cached query data
+    /** OPTIMISTIC UPDATING */
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previousHeroData = queryClient.getQueryData("super-heroes");
+      // so this updates the list of heroes on the UI before even making the post request.
+      // that's the optimism!
       queryClient.setQueryData("super-heroes", (oldQueryData) => {
         return {
           ...oldQueryData,
-          data: [...oldQueryData.data, data.data],
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
         };
       });
+      // this is to roll back data in case the network request errors out
+      return {
+        previousHeroData,
+      };
     },
+    onError: (_error, _hero, context) => {
+      // this is the rollback function on error
+      queryClient.setQueryData("super-heroes", context.previousHeroData);
+    },
+    onSettled: () => {
+      // refetch from the server.
+      // the user will notice nothing. the fetch occurs in the background and simply syncs the data
+      queryClient.invalidateQueries("super-heroes");
+    },
+
+    /** INVALIDATE QUERY, OR UPDATE QUERY CACHE */
+    // //steps to not making an additional network request once mutation succeeds:
+    // // step 1: use param 'data' (which is the entire response for post request)
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries("super-heroes"); < this is a way to refetch data as soon as the mutation is successful
+    //   // however, this is an additional network request.
+    //   // to save a network request we have to do something else.
+    //   //step 2: update query cache using setQueryData. oldQueryData = cached query data
+    //   queryClient.setQueryData("super-heroes", (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    // });
+    // },
   });
 };
